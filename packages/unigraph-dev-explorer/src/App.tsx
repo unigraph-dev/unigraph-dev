338cacb4c815148824f0342823b2e540139c267a
Steven Salka <stevensalka925@gmail.com>
2020-11-12 23:27:25 -0800

    add Roboto font

--- packages/unigraph-dev-explorer/src/App.tsx
+++ packages/unigraph-dev-explorer/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'typeface-roboto';
 
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
