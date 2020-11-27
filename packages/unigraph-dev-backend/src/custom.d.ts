d734d7c50358dc4c0278080a5a24b1b0c2be1cda
Haoji Xu <haoji.xu@outlook.com>
2020-11-27 23:49:32 +0800

    feat(backend): add server api to insert unigraph objects

--- packages/unigraph-dev-backend/src/custom.d.ts
+++ packages/unigraph-dev-backend/src/custom.d.ts
@@ -12,3 +12,57 @@ declare type UnigraphUpsert = {
   queries: string[],
   mutations: any[]
 }
+
+declare type EventCreateUnigraphSchema = {
+  "type": "event",
+  "event": "create_unigraph_schema",
+  schema: object | object[],
+  id: number
+}
+
+declare type EventCreateUnigraphObject = {
+  "type": "event",
+  "event": "create_unigraph_object",
+  object: object,
+  id: number,
+  schema: string | undefined,
+  padding: boolean | undefined
+}
+
+declare type EventDropAll = {
+  type: "event",
+  event: "drop_all",
+  id: number
+}
+
+declare type EventDropData = {
+  type: "event",
+  event: "drop_data",
+  id: number
+}
+
+declare type EventCreateDataByJson = {
+  type: "event",
+  event: "create_data_by_json",
+  id: number,
+  data: object
+}
+
+declare type EventSetDgraphSchema = {
+  type: "event",
+  event: "set_dgraph_schema",
+  id: number,
+  schema: string
+}
+
+declare type EventQueryByStringWithVars = {
+  type: "event",
+  event: "query_by_string_with_vars",
+  vars: object,
+  query: string,
+  id: number
+}
+
+declare interface IWebsocket {
+  send: Function
+}
