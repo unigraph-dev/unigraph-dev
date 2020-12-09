10d6596c2156c125782d6d29a996e192ececdcf8
Steven Salka <stevensalka925@gmail.com>
2020-12-08 22:37:39 -0800

    make WS request through AddSchema component

--- packages/unigraph-dev-explorer/src/pages/AddSchema.tsx
+++ packages/unigraph-dev-explorer/src/pages/AddSchema.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
-import SchemaEditor from '../components/SchemaEditor';
+import React, { useState, useCallback } from 'react';
 import Typography from '@material-ui/core/Typography';
+import * as uuid from 'uuid';
+
+import SchemaEditor from '../components/SchemaEditor';
+import { makeUnigraphId, makeRefUnigraphId } from '../components/SchemaEditor/types';
 
 const AddSchema = () => {
+  const [schemaResult, setSchemaResult] = useState<any>();
+  const createSchema = useCallback((schema: any) => {
+    const id = uuid.v4();
+
+    const handleResponse = ({ data }: MessageEvent) => {
+      const res = JSON.parse(data);
+      if (res.id !== id) return;
+
+      // TODO: either return complete schema from backend or get its UID and make a separate request for it here
+      // - the res here just has metadata (type/success/request id)
+      setSchemaResult(res);
+
+      window.unigraph.backendConnection.removeEventListener('message', handleResponse);
+    };
+
+    window.unigraph.backendConnection.addEventListener('message', handleResponse);
+
+    // TODO: make this more scalable (right now, handleResponse will receive every
+    // message sent between when schema is submitted and when response is sent back)
+    window.unigraph.backendConnection.send(JSON.stringify({
+      type: 'event',
+      event: 'create_unigraph_schema',
+      schema,
+      id,
+    }));
+  }, []);
+
   return (
     <div>
       <Typography gutterBottom variant="h5">
         Add Schema
       </Typography>
-      <SchemaEditor />
+      <SchemaEditor onSubmit={createSchema} />
+      {schemaResult && (
+        <pre>{JSON.stringify(schemaResult, null, 2)}</pre>
+      )}
     </div>
   );
 }
