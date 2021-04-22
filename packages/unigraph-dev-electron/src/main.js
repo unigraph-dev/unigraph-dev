e68dd7710253540e706bf3524fce6e674029fd20
Sophia Xu <sophia.xu@mail.mcgill.ca>
2021-04-22 17:52:53 -0400

    feat: initial electron support

---
+++ packages/unigraph-dev-electron/src/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, '..', 'public', 'preload.js')
+    }
+  })
+
+  win.loadFile(path.join(__dirname, '..', 'public', 'index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
