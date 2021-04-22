68e1cd541ca9e5b957e1d5bc02dfa7df1a165741
Sophia Xu <jingyi@jingyixu.me>
2021-04-23 07:12:30 +0800

    feat: package script

--- scripts/prepare_for_electron.sh
+++ scripts/prepare_for_electron.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-mkdir packages/unigraph-dev-electron/dist
+rm -rf packages/unigraph-dev-electron/build
+rm -rf packages/unigraph-dev-electron/dist
+yarn build-common
+
+# copy frontend code
 yarn workspace unigraph-dev-explorer build
+cp -r packages/unigraph-dev-explorer/build packages/unigraph-dev-electron/build
+
+# copy backend code
+yarn workspace unigraph-dev-backend build
+cp -r packages/unigraph-dev-backend/dist packages/unigraph-dev-electron/dist
+
+# copy dgraph
+mkdir packages/unigraph-dev-electron/dgraph
+cp /opt/unigraph/dgraph packages/unigraph-dev-electron/dgraph
