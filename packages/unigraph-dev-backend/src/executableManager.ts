81d308c8db9f6f6ae226bca93102f0508c16bd2e
Sophia Xu <sophia.xu@mail.mcgill.ca>
2021-05-02 02:58:21 -0400

    fix: syntax

--- packages/unigraph-dev-backend/src/executableManager.ts
+++ packages/unigraph-dev-backend/src/executableManager.ts
@@ -65,7 +65,7 @@ export function buildExecutable(exec: Executable, context: ExecContext, unigraph
 export function initExecutables(executables: Executable[], context: ExecContext, unigraph: Unigraph, schedule: Record<string, cron.ScheduledTask>) {
     executables.forEach(el => {
         if (el.periodic) {
-            schedule[el["unigraph.id"]].destroy();
+            schedule[el["unigraph.id"]]?.destroy();
             schedule[el["unigraph.id"]] = cron.schedule(el.periodic, buildExecutable(el, context, unigraph))
         }
     })
