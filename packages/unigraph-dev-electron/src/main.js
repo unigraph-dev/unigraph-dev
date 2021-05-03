9e85e59c2e493c9b2e50b576cf4b32e49939ca5d
Sophia Xu <jingyi@jingyixu.me>
2021-05-04 00:55:06 +0800

    feat: electron persistence and minimize to tray

--- packages/unigraph-dev-electron/src/main.js
+++ packages/unigraph-dev-electron/src/main.js
@@ -1,17 +1,28 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, Tray, nativeImage } = require('electron')
 const path = require('path')
 const { spawn } = require("child_process");
 const { fixPathForAsarUnpack } = require('electron-util');
 
 const userData = app.getPath('userData')
 
-let alpha = spawn(fixPathForAsarUnpack(path.join(__dirname, '..', 'dgraph', 'dgraph')),  ["alpha", '--wal', path.join(userData, 'w'), '--postings', path.join(userData, 'p')])
+let logs = [];
+let mainWindow = null;
+
+let oldConsoleLog = console.log;
+console.log = (...data) => { logs.push(...data); checkIfUComplete(...data); return oldConsoleLog(...data) }
+
+let alpha = spawn(fixPathForAsarUnpack(path.join(__dirname, '..', 'dgraph', 'dgraph')), ["alpha", '--wal', path.join(userData, 'w'), '--postings', path.join(userData, 'p')])
 let zero = spawn(fixPathForAsarUnpack(path.join(__dirname, '..', 'dgraph', 'dgraph')), ["zero", '--wal', path.join(userData, 'zw')])
 
 let completedLog = "ResetCors closed" // When this is logged we know it's completed
+let completedULog = "Unigraph server listening on port"
 
-function checkIfComplete (str) {
+function checkIfComplete(str) {
   if (str.includes && str.includes(completedLog)) dgraphLoaded();
+}
+
+function checkIfUComplete(str) {
+  if (str.includes && str.includes(completedULog)) unigraphLoaded();
 }
 
 alpha.stdout.on('data', function (data) {
@@ -34,7 +45,22 @@ zero.stderr.on('data', function (data) {
   checkIfComplete(data.toString())
 });
 
-function createMainWindow () {
+function createMainWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, '..', 'src', 'preload.js'),
+      nativeWindowOpen: true
+    }
+  })
+
+  win.loadFile(path.join(__dirname, '..', 'buildweb', 'loading_bar.html'))
+
+  return win;
+}
+
+function createMainWindowNoLoad() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -45,34 +71,56 @@ function createMainWindow () {
   })
 
   win.loadFile(path.join(__dirname, '..', 'buildweb', 'index.html'))
+
+  return win;
 }
 
-function dgraphLoaded () {
+function dgraphLoaded() {
   let index = require(path.join(__dirname, '..', 'distnode', 'index.js'))
-
-  app.whenReady().then(() => {
-    setTimeout(() => {
-      createMainWindow()
-  
-      app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-          createMainWindow()
-        }
-      })
-    }, 5000)
-  })
-  
-  
 }
 
+function unigraphLoaded() {
+  if (mainWindow) mainWindow.loadFile(path.join(__dirname, '..', 'buildweb', 'index.html'))
+}
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.whenReady().then(() => {
+  tray = new Tray(nativeImage.createEmpty())
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  tray.setToolTip('Unigraph')
+  tray.setContextMenu(contextMenu)
+  setTimeout(() => {
+    mainWindow = createMainWindow()
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindowNoLoad()
+      }
+    })
+  }, 0)
 })
 
-app.on('before-quit', () => {
+app.on('window-all-closed', () => {
+  // Prevent app quitting
+})
+
+app.on('quit', () => {
   alpha.kill();
   zero.kill();
 })
+
+app.on('will-quit', async function (e) {
+  var choice = await require('electron').dialog.showMessageBox({
+    type: 'question',
+    buttons: ['Quit', 'Minimize to Tray'],
+    title: 'Confirm',
+    message: 'Unigraph can minimize to tray. It consumes minimal battery and can help you manage your knowledge more quickly (for example, instant access to your information).'
+  })
+  if (choice.response === 1) {
+    e.preventDefault();
+  }
+});
