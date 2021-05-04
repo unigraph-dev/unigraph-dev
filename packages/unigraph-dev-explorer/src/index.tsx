8c4f9547abcbf9ba4d34d6af38d54c038fa3c2eb
Sophia Xu <sophia.xu@mail.mcgill.ca>
2021-05-04 09:20:50 -0400

    feat: add global notification center

--- packages/unigraph-dev-explorer/src/index.tsx
+++ packages/unigraph-dev-explorer/src/index.tsx
@@ -12,6 +12,8 @@ import App from './App';
 import { SplashScreen, DisconnectedSplashScreen} from './pages/SplashScreen';
 import { WorkSpace } from './Workspace';
 import { UserSettings } from './global';
+import { ANotification } from './components/UnigraphCore/Notification';
+import { unpad } from 'unigraph-dev-common/lib/utils/entityUtils';
 
 let hst = window.location.hostname.length ? window.location.hostname : "localhost";
 
@@ -19,9 +21,16 @@ let hst = window.location.hostname.length ? window.location.hostname : "localhos
 window.DynamicViews = DynamicViews;
 window.DynamicViewsDetailed = DynamicViewsDetailed;
 
+window.notificationCallbacks = [];
+window.registerNotifications = (callback: ((data: any[]) => any)) => {
+  callback(window.notifications);
+  window.notificationCallbacks.push(callback)
+}
+
 const defaultSettings: UserSettings = {
   serverLocation: `ws://${hst}:3001`,
-  "new-window": "new-tab"
+  "new-window": "new-tab",
+  nativeNotifications: false
 }
 
 let userSettings = defaultSettings;
@@ -52,6 +61,22 @@ if (window.location.pathname === '/pages') {
   </React.StrictMode>,);
   
   window.unigraph.backendConnection.onopen = () => {
+    // Register notification center
+    // TODO: Do we need a state management library? Ask around and evaluate.
+    window.unigraph.subscribeToType("$/schema/notification", (data: any[]) => {
+      window.notifications = data;
+      window.notificationCallbacks.forEach(el => el(data));
+    }).then(() => {
+      if (isJsonString(window.localStorage.getItem('userSettings')) && // @ts-expect-error: Already checked for nullity
+        JSON.parse(window.localStorage.getItem('userSettings'))['nativeNotifications']) {
+          window.notificationCallbacks.push((el: any) => {
+            const unpadded: ANotification = unpad(el); 
+            console.log(unpadded);
+            const nfn = new Notification(unpadded.name, {body: unpadded.from + ": " + unpadded.content})
+          })
+      }
+    })
+
     render(<React.StrictMode>
       <WorkSpace />
     </React.StrictMode>,);
