3c06ba4ea885fb22d2c261efdb6312abd8b74f29
Sophia Xu <sophia.xu@mail.mcgill.ca>
2021-05-11 10:48:25 -0400

    fix: grpc message length limits and type declaration

--- packages/unigraph-dev-explorer/src/react-app-env.d.ts
+++ packages/unigraph-dev-explorer/src/react-app-env.d.ts
@@ -3,3 +3,8 @@ declare module "react/jsx-runtime" {
     export default any;
 } // TODO: Versioning chores, this is a workaround until better typescript declaration are released.
 // See: https://github.com/facebook/create-react-app/issues/10109
+
+declare module '*.pkg' {
+    const pkg: any;
+    export { pkg };
+}
