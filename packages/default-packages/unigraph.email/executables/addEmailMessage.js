ebb05ce2f8ef1e581f10dffa33d8e44007e52c4d
Sophia Xu <jingyi@jingyixu.me>
2021-05-18 23:44:06 +0800

    feat: basic email messages ui

--- packages/default-packages/unigraph.email/executables/addEmailMessage.js
+++ packages/default-packages/unigraph.email/executables/addEmailMessage.js
@@ -3,21 +3,42 @@ const simpleParser = require('mailparser').simpleParser;
 
 const parsed = await Promise.all(msgs.map(it => simpleParser(it.message)));
 
+const getQuery = (msgid) => `(func: type(Entity)) @cascade { 
+    uid
+	type @filter(eq(<unigraph.id>, "$/schema/email_message"))
+    _value {
+      message_id @filter(eq(<_value.%>, "${msgid}"))
+    }
+}`
+
 const dest = parsed.map((el) => {
     return {
         name: el.subject,
         message_id: el.messageId,
         message: {
             date_received: el.date.toISOString(),
-            sender: [el.from?.text],
-            recipient: [el.to?.text],
+            sender: [el.from?.text].filter(it => it !== undefined),
+            recipient: [el.to?.text].filter(it => it !== undefined),
         },
         content: {
-            text: el.html
+            abstract: el?.text?.slice(0, 100) || "No preview available",
+            text: el.html || el.textAsHtml || el.text
         }
     }
 })
 
-console.log(dest[0]);
-await unigraph.addObject(dest[0], '$/schema/email_message');
-await unigraph.addObject(dest[0], '$/schema/email_message');
+const results = await unigraph.getQueries(dest.map(el => getQuery(el.message_id)));
+
+let inbox_els = []
+let count = 0
+
+for (let i=0; i<dest.length; ++i) {
+    if (results[i].length === 0) {
+        count ++;
+        const uid = await unigraph.addObject(dest[i], '$/schema/email_message');
+        if (msgs[i]?.read === false) inbox_els.push(uid[0]);
+    }
+}
+
+await unigraph.runExecutable("$/executable/add-item-to-list", {where: "$/entity/inbox", item: inbox_els.reverse()});
+setTimeout(() => unigraph.addNotification({name: "Inboxes synced", from: "unigraph.email", content: "Added " + count + " items.", actions: []}), 1000);
--- packages/unigraph-dev-explorer/src/examples/email/Email.tsx
+++ packages/unigraph-dev-explorer/src/examples/email/Email.tsx
@@ -1,8 +1,25 @@
-import { List, ListItem } from "@material-ui/core";
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from "@material-ui/core";
 import React from "react";
 import { registerDynamicViews, withUnigraphSubscription } from "unigraph-dev-common/lib/api/unigraph-react"
 import { pkg as emailPackage } from 'unigraph-dev-common/lib/data/unigraph.email.pkg';
+import { unpad } from "unigraph-dev-common/lib/utils/entityUtils";
 import { AutoDynamicView } from "../../components/ObjectView/DefaultObjectView";
+import { DynamicViewRenderer } from "../../global";
+import * as timeago from 'timeago.js';
+
+type AEmail = {
+    name: string,
+    message_id: string,
+    message: {
+        date_received: string,
+        sender: string[],
+        recipient: string[]
+    },
+    content: {
+        text: string,
+        abstract: string
+    }
+}
 
 const EmailListBody: React.FC<{data: any[]}> = ({data}) => {
 
@@ -14,6 +31,18 @@ const EmailListBody: React.FC<{data: any[]}> = ({data}) => {
         </List>
     </div>
 }
+
+const EmailMessage: DynamicViewRenderer = ({data, callbacks}) => {
+    let unpadded: AEmail = unpad(data);
+    console.log(unpadded);
+    return <ListItem>
+        <ListItemAvatar><Avatar>{unpadded.message.sender[0][0]}</Avatar></ListItemAvatar>
+        <ListItemText primary={unpadded.name} secondary={unpadded.content?.abstract+"..."}></ListItemText>
+        <ListItemText style={{flex: "none"}} secondary={timeago.format(new Date(unpadded?.message?.date_received))}></ListItemText>
+    </ListItem>
+}
+
+registerDynamicViews({'$/schema/email_message': EmailMessage})
 
 export const EmailList = withUnigraphSubscription(
     EmailListBody,
