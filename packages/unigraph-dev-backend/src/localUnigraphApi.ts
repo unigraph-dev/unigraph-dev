70fc0898f370c1267565b5d2a8892247522c1106
Sophia Xu <jingyi@jingyixu.me>
2021-06-09 19:12:25 +0800

    chore: fix lint

--- packages/unigraph-dev-backend/src/localUnigraphApi.ts
+++ packages/unigraph-dev-backend/src/localUnigraphApi.ts
@@ -109,7 +109,7 @@ export function getLocalUnigraphAPI(client: DgraphClient, states: {caches: Recor
               
                 return resultArray
               }, [] as any)
-            let res: any[] = [];
+            const res: any[] = [];
             for (let i=0; i<batchedQueries.length; ++i) {
                 res.push(...(await client.queryDgraph(`query {${batchedQueries[i].join('\n')}}`)))
             }
