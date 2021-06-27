fa7cfcf9119aae28a8d8556606c79db2be97561e
Sophia Xu <jingyi@jingyixu.me>
2021-06-27 14:03:29 +0800

    fix: graph view ux

--- packages/unigraph-dev-explorer/src/components/ObjectView/GraphView.tsx
+++ packages/unigraph-dev-explorer/src/components/ObjectView/GraphView.tsx
@@ -4,6 +4,19 @@ import ForceGraph2D from 'react-force-graph-2d';
 import { SizeMe } from "react-sizeme";
 import _ from "lodash";
 
+const queryNameIndex = `@filter((NOT eq(<_hide>, true)) AND (NOT eq(<_propertyType>, "inheritance"))) {
+    uid 
+    unigraph.indexes {
+        uid
+        name {
+            uid 
+            expand(_userpredicate_) { uid expand(_userpredicate_) { uid expand(_userpredicate_) { 
+                uid expand(_userpredicate_) { uid expand(_userpredicate_) { uid expand(_userpredicate_) } } } } }
+        }
+    }
+    <type> { <unigraph.id> }
+}`
+
 export const GraphView = ({uid}: any) => {
     const [entities, setEntities] = React.useState<any>([]);
 
@@ -12,38 +25,16 @@ export const GraphView = ({uid}: any) => {
         
         // name can be either plain or interface
         window.unigraph.subscribeToQuery(`(func: uid(${uid})) {
-            <unigraph.origin> @filter((NOT eq(<_hide>, true)) AND (NOT eq(<_propertyType>, "inheritance"))) {
-                uid 
-                unigraph.indexes {
-                    uid
-                    name {
-                        uid 
-                        expand(_userpredicate_) { uid expand(_userpredicate_) { uid expand(_userpredicate_) { 
-                            uid expand(_userpredicate_) { uid expand(_userpredicate_) { uid expand(_userpredicate_) } } } } }
-                    }
-                }
-                <type> { <unigraph.id> }
-            }
-            <~unigraph.origin> @filter((NOT eq(<_hide>, true)) AND (NOT eq(<_propertyType>, "inheritance"))) {
-                uid 
-                unigraph.indexes {
-                    uid
-                    name {
-                        uid 
-                        expand(_userpredicate_) { uid expand(_userpredicate_) { uid expand(_userpredicate_) { 
-                            uid expand(_userpredicate_) { uid expand(_userpredicate_) { uid expand(_userpredicate_) } } } } }
-                    }
-                }
-                <type> { <unigraph.id> }
-            }
+            <unigraph.origin> ${queryNameIndex}
+            <~unigraph.origin> ${queryNameIndex}
         }`, (res: any) => {
             console.log(res);
-            const [entitiesInto, entitiesOutof] = ([res[0]['unigraph.origin'], res[0]['~unigraph.origin']]).map(el => el.filter((el: any) => el.type !== undefined).map((el: any) => { return {
+            const [entitiesInto, entitiesOutof] = ([res[0]['unigraph.origin'], res[0]['~unigraph.origin']]).map(el => el?.filter((el: any) => el.type !== undefined).map((el: any) => { return {
                 id: el.uid,
                 type: el['type']?.['unigraph.id'],
                 name: (new UnigraphObject(el?.['unigraph.indexes']?.['name'])).as("primitive") || "No name"
             }}))
-            setEntities(_.uniqBy([...entitiesInto, ...entitiesOutof], el => el.id));
+            setEntities(_.uniqBy([...(entitiesInto || []), ...(entitiesOutof || [])], el => el.id));
         }, id, true);
 
         return function cleanup () { window.unigraph.unsubscribe(id); }
@@ -54,7 +45,24 @@ export const GraphView = ({uid}: any) => {
         <SizeMe>{({size}) => <ForceGraph2D 
             width={size.width || undefined} height={size.height || undefined}
             nodeAutoColorBy={(node) => (node as any).type}
-            nodeLabel={(node) => (node as any).name}
+            nodeLabel={(node: any) => `Name: ${node.name}\n Type: ${node.type}`}
+            nodeCanvasObject={(node, ctx, globalScale) => {
+                const label = (node as any).name;
+                const fontSize = 12/globalScale;
+                ctx.font = `${fontSize}px Sans-Serif`;
+                const textWidth = typeof label === "string" ? ctx.measureText(label).width : 10;
+                const [w, h] = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+    
+                ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+                ctx.fillRect((node.x || 0) - w / 2, (node.y || 0) - h / 2, w, h);
+    
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
+                ctx.fillStyle = ((node as any).color || "");
+                ctx.fillText(label?.toString() || "", node.x || 0, node.y || 0);
+    
+                (node as any).__bckgDimensions = [w, h]; // to re-use in nodePointerAreaPaint
+            }}
             graphData={{nodes: entities, links: entities.map((el: any) => {return {source: uid, target: el['id']}})}} 
         />}</SizeMe>
     </div>
