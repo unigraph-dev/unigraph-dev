bdf69fb8d8bd83a8722b77726e655e10a46b3040
Sophia Xu <jingyi@jingyixu.me>
2021-07-23 03:03:58 +0800

    fix: html view normalizes images

--- packages/unigraph-dev-explorer/src/examples/semantic/Html.tsx
+++ packages/unigraph-dev-explorer/src/examples/semantic/Html.tsx
@@ -1,9 +1,15 @@
+import React from "react";
 import { AutoDynamicView } from "../../components/ObjectView/DefaultObjectView"
 import { DynamicViewRenderer } from "../../global"
 
 export const Html: DynamicViewRenderer = ({data, context, callbacks}) => {
+    const frm = React.useRef(null);
+
     return <div style={{width: "100%", height: "100%", display: "flex", flexDirection: "column"}}>
         {context ? <AutoDynamicView object={context} /> : []}
-        <iframe srcDoc={data['_value.%']} style={{flexGrow: 1, width: "100%"}}/>
+        <iframe srcDoc={data['_value.%']} style={{flexGrow: 1, width: "100%"}} ref={frm} onLoad={() => {
+            console.log(frm.current);
+            (frm.current as any).contentDocument.head.insertAdjacentHTML("beforeend", "<style>img{ width: 100%; height: auto } video{ width: 100%; height: auto }</style>");
+        }}/>
     </div>
 }
