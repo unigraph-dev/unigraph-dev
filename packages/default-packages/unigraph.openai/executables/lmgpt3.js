2dc8309da8d31c24d62322fef83a9b693bcc4b7e
Sophia Xu <sophia.xu@mail.mcgill.ca>
2021-09-10 12:34:29 -0400

    feat: add openai api

---
+++ packages/default-packages/unigraph.openai/executables/lmgpt3.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+const { engine, prompt, temperature, max_tokens, top_p, frequency_penalty, presence_penalty } = context.params;
+const apikey = unigraph.getSecret("openai", "api_key");
+
+const apiurl = "https://api.openai.com/v1/engines/" + (engine || "davinci") + "/completions"
+
+const response = await fetch(apiurl, {
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + apikey
+    },
+    method: 'POST',
+    body: JSON.stringify({
+        prompt: prompt,
+        temperature: temperature || 0.7,
+        max_tokens: max_tokens || 64,
+        top_p: top_p || 1,
+        frequency_penalty: frequency_penalty || 0,
+        presence_penalty: presence_penalty || 0
+    })
+})
+
+const data = await response.json();
+console.log(data);
+return data.choices;
