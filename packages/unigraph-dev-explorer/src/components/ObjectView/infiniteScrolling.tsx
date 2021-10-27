d318e60f26fe2733d84ad86c4cb5578a0dd5ed45
Sophia Xu <sophia.xu@mail.mcgill.ca>
2021-10-27 14:30:56 -0400

    feat: modularize infinite scrolling lists

---
+++ packages/unigraph-dev-explorer/src/components/ObjectView/infiniteScrolling.tsx
@@ -0,0 +1,48 @@
+import _ from "lodash"
+import { getRandomInt } from "unigraph-dev-common/lib/api/unigraph";
+
+/**
+ * Manages the data side of an infinite scrolling view.
+ * Deals with subscribing/unsubscribing objects in response to the user's action.
+ * @param uids A list of UIDs in string format consisting all the items.
+ * @param chunk Number, default to 50, meaning how much objects to get in a given subscription.
+ * @param stateCallback A callback that would be called every time loaded items are updated.
+ */
+export const setupInfiniteScrolling = (uids: string[], chunk = 50, stateCallback: (loadedItems: any[]) => void) => {
+
+    const states = {
+        subs: [] as any[],
+        chunks: _.chunk(uids, chunk),
+    }
+
+    const onStateUpdated = () => {
+        const items = states.subs.reduce((prev: any[], curr: any) => [...prev, ...curr.result], []);
+        stateCallback(items);
+    }
+
+    const onUserNext = () => {
+        const [subsHead, chunksHead] = [states.subs.length, states.chunks.length];
+        if (subsHead < chunksHead) {
+            const toSub = states.chunks[subsHead];
+            const subsId = getRandomInt();
+            states.subs.push({id: subsId, result: []});
+            window.unigraph.subscribeToObject(toSub, (results: any[]) => {
+                let uidsMap: any = {};
+                results.forEach((el: any) => {uidsMap[el.uid] = el});
+                states.subs[subsHead].result = toSub.map(el => uidsMap[el]);
+                onStateUpdated();
+            }, subsId);
+        }
+    }
+
+    const onCleanup = () => {
+        states.subs.forEach(el => window.unigraph.unsubscribe(el.id));
+    }
+
+    const returns = {
+        next: onUserNext,
+        cleanup: onCleanup
+    }
+
+    return returns;
+}
--- packages/unigraph-dev-explorer/src/components/UserLibrary/UserLibraryAll.tsx
+++ packages/unigraph-dev-explorer/src/components/UserLibrary/UserLibraryAll.tsx
@@ -5,105 +5,47 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import { useEffectOnce } from 'react-use';
 import { buildGraph, getRandomInt, UnigraphObject } from 'unigraph-dev-common/lib/api/unigraph';
 import { AutoDynamicView } from '../ObjectView/DefaultObjectView';
+import { setupInfiniteScrolling } from '../ObjectView/infiniteScrolling';
 
 const UserLibraryAll = ({id}: any) => {
 
-    const [data, setData] = React.useState<UnigraphObject[]>([]);
-    const [loadedItems, setLoadedItems] = React.useState<UnigraphObject[]>([]);
-    const [subs, setSubs] = React.useState<any[]>([]);
-    const subsRef = React.useRef<any>([]);
-    const [reload, setReload] = React.useState(0);
-    const [chunks, setChunks] = React.useState<any[][]>(_.chunk(data, 100));
-
-    React.useEffect(() => { setChunks(_.chunk(data, 100)) }, [data]);
-
-    React.useEffect(() => { 
-        subsRef.current = subs;
-        if (!(loadedItems.length && !subs.length)) { 
-            const newItems = subs.reduce((prev: any[], current: any) => {
-                prev.push(...current.results); 
-                return prev;
-            }, []);
-            setLoadedItems(buildGraph(newItems).filter((el: any) => 
-                (Object.keys(window.unigraph.getState('registry/dynamicView').value)
-                    .includes(el?.type?.['unigraph.id'])) && el?.type?.['unigraph.id'] !== "$/schema/markdown"));
-        }
-    }, [subs]);
-
-    React.useEffect(() => {
-        if (reload !== 0 && subs.length === 0) {
-            for(let i=0; i<reload; ++i) {
-                nextGroup();
-            };
-            setReload(0);
-        }
-    }, [subs, reload])
-
-    React.useEffect(() => {console.log(loadedItems)}, [loadedItems])
+    const [data, setData] = React.useState<string[]>([]);
+    const [loadedItems, setLoadedItems] = React.useState<any[]>([]);
+    const [setupProps, setSetupProps] = React.useState<{next: any, cleanup: any} | null>(null);
 
     useEffectOnce(() => {
         const subsId = getRandomInt();
-        window.unigraph.subscribeToType("any", (result: any[]) => {setData(result)}, subsId, {uidsOnly: true});
+        window.unigraph.subscribeToType("any", (result: any[]) => {setData(result.map(el => el.uid))}, subsId, {uidsOnly: true});
 
         return function cleanup() { window.unigraph.unsubscribe(subsId); }
     })
 
-    const setSubResult = React.useCallback((data: any[], index: number) => {
-        let newSubs = [...subsRef.current];
-        if (newSubs[index]) {
-            newSubs[index].results = data;
-            setSubs(newSubs)
-        };
-    }, [subs]);
-    const nextGroup = () => {
-        console.log("Showing next group...", subs.length)
-        if (chunks[subs.length]?.length) {
-            const subId = getRandomInt();
-            const thisSub = {id: subId, results: [] };
-            console.log(chunks[subs.length].map((el: any) => el.uid));
-            window.unigraph.subscribeToObject(chunks[subs.length].map((el: any) => el.uid), (results: any[]) => {
-                let resultsMap: any = {};
-                results.forEach((el: any) => resultsMap[el.uid] = el);
-                setSubResult(chunks[subs.length].map((el: any) => resultsMap[el.uid]), subs.length);
-            }, subId);
-            setSubs([...subs, thisSub]);
-        }
-    };
-
     React.useEffect(() => {
-        const nextGroups = subs.length || 1;
-        const cleanup = () => {
-            subsRef.current.forEach((el: any) => {
-                window.unigraph.unsubscribe(el.id);
-        })};
-        cleanup();
-        setSubs([]);
-        setReload(nextGroups);
-        return cleanup;
-    }, [chunks])
+        if (setupProps?.cleanup) setupProps.cleanup();
+        if (data.length) {
+            const newProps = setupInfiniteScrolling(data, 100, (items: any[]) => {
+                setLoadedItems(items);
+            });
+            setSetupProps(newProps);
+            newProps.next();
+        }
 
-    /*
-    return <div>
-        <List>
-            {data.map(it => <ListItem button key={it.uid}>
-                <AutoDynamicView object={it} />
-            </ListItem>)}
-        </List>
-    </div>*/
+        return function cleanup () { setupProps?.cleanup() }
+    }, [data])
+    
+
     return <div>
         <Typography gutterBottom variant="h4">
             Library - Recent items
         </Typography>
         <InfiniteScroll
             dataLength={loadedItems.length} //This is important field to render the next data
-            next={nextGroup}
+            next={setupProps?.next || (() => {})}
             scrollableTarget={"workspaceContainer"+id}
-            hasMore={true}
+            hasMore={loadedItems.length < data.length}
             loader={<h4>Loading...</h4>}
             endMessage={
-                <p style={{ textAlign: 'center' }}>
-                <b>Yay! You have seen it all</b>
-                </p>
+                <React.Fragment/>
             }
         >
             {buildGraph(loadedItems || []).map((it: any) => <ListItem button key={it.uid}>
