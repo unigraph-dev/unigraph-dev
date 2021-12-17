810b1a07bf392a621cf109115aea4c5a94816a27
Adrian Sampaleanu <nmanole@gmail.com>
2021-12-16 23:32:51 -0500

    Make some npm scripts work with PowerShell.

---
+++ scripts/build_default_packages.ps1
@@ -0,0 +1,10 @@
+mkdir -Force packages/unigraph-dev-common/lib
+mkdir -Force packages/unigraph-dev-common/lib/data
+
+Get-ChildItem -Path ./packages/default-packages/* |
+
+Foreach-Object {
+  #Do something with $_.FullName
+  Write-Output "building $_.FullName" 
+  node ./scripts/unigraph-packager.js $_.FullName -o ./packages/unigraph-dev-common/lib/data
+}
