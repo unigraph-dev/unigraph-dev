30f38806e91075799f317207dbab8c22df96cbf4
Sophia Xu <sophia.xu@mail.mcgill.ca>
2021-12-21 03:06:33 -0500

    feat(code editor): group executables by package

--- packages/unigraph-dev-explorer/src/components/UnigraphCore/CodeEditor.tsx
+++ packages/unigraph-dev-explorer/src/components/UnigraphCore/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, InputLabel, List, ListItem, MenuItem, Select, Tab, Tabs, TextField } from '@material-ui/core';
+import { Button, Collapse, FormControl, InputLabel, List, ListItem, ListItemText, MenuItem, Select, Tab, Tabs, TextField } from '@material-ui/core';
 import { TabPanel } from './TabPanel';
 import React from 'react';
 import SplitterLayout from 'react-splitter-layout';
@@ -8,6 +8,8 @@ import { getRandomInt } from 'unigraph-dev-common/lib/api/unigraph';
 import { AutoDynamicView } from '../ObjectView/AutoDynamicView';
 import DetailedObjectView from '../UserLibrary/UserLibraryObject';
 import { ExecutableCodeEditor } from '../ObjectView/DefaultCodeEditor';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import _ from 'lodash';
 
 export const NewUserCode = ({}) => {
     const [displayName, setDisplayName] = React.useState("");
@@ -41,12 +43,15 @@ export const CodeEditor = ({id}: any) => {
 
     const [execcontent, setexecContent]: any = React.useState([]);
     const [userExecContent, setUserExecContent]: any = React.useState([]);
+    const [execPackages, setExecPackages]: any = React.useState([]);
     const [currentUid, setCurrentUid]: any = React.useState("");
 
     const [currentTab, setCurrentTab]: any = React.useState(0);
 
     const currentView = <DetailedObjectView uid={currentUid} id={id} component={{"$/schema/executable": {view: ExecutableCodeEditor}}} />
 
+    const [isUserCollapseOpen, setIsUserCollapseOpen] = React.useState(false);
+    const [currentPackage, setCurrentPackage] = React.useState('');
 
     useEffectOnce(() => {
         const id = getRandomInt();
@@ -59,6 +64,8 @@ export const CodeEditor = ({id}: any) => {
             });
             setexecContent(namedEx);
             setUserExecContent(userEx);
+            const packages = _.uniq(namedEx.map(el => el['unigraph.id'].split('/').slice(0, 3).join('/'))).sort();
+            setExecPackages(packages);
         }, id)
 
         return function cleanup() {
@@ -68,28 +75,33 @@ export const CodeEditor = ({id}: any) => {
 
     return <SplitterLayout primaryIndex={1} secondaryInitialSize={360}>
         <div>
-            <Tabs value={currentTab} onChange={(ev, newTab) => {setCurrentTab(newTab)}} aria-label="simple tabs example"
-                style={{position: "fixed", height: "48px", backgroundColor: "white", zIndex: 999}}
-            >
-                <Tab label="Packaged code" />
-                <Tab label="User code" />
-            </Tabs>
-            <div style={{minHeight: "48px"}}></div>
-            <TabPanel value={currentTab} index={0}>
-                <List style={{overflow: "auto"}}>
-                    {execcontent.map((it: any) => <ListItem key={it.uid} selected={currentUid === it.uid} onClick={() => {setCurrentUid(it.uid)}}>
-                        <AutoDynamicView object={it} />
-                    </ListItem>)}
-                </List>
-            </TabPanel>
-            <TabPanel value={currentTab} index={1}>
+            {/* User code */}
+            <ListItem onClick={() => {setIsUserCollapseOpen(!isUserCollapseOpen)}} style={{cursor: "pointer"}}>
+                <ListItemText primary="User code" />
+                {isUserCollapseOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={isUserCollapseOpen}>
                 <NewUserCode />
                 <List style={{overflow: "auto"}}>
                     {userExecContent.map((it: any) => <ListItem key={it.uid} selected={currentUid === it.uid} onClick={() => {setCurrentUid(it.uid)}}>
                         <AutoDynamicView object={it} />
                     </ListItem>)}
                 </List>
-            </TabPanel>
+            </Collapse>
+            
+            {execPackages.map((el: string) => <React.Fragment>
+                <ListItem onClick={() => {setCurrentPackage(currentPackage === el ? "" : el)}} style={{cursor: "pointer"}}>
+                    <ListItemText primary={el} />
+                    {currentPackage === el ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={currentPackage === el}>
+                    <List style={{overflow: "auto"}}>
+                        {execcontent.filter((it: any) => it['unigraph.id']?.startsWith(el)).map((it: any) => <ListItem key={it.uid} selected={currentUid === it.uid} onClick={() => {setCurrentUid(it.uid)}}>
+                            <AutoDynamicView object={it} />
+                        </ListItem>)}
+                    </List>
+                </Collapse>
+            </React.Fragment>)}
         </div>
         <div>{currentUid ? currentView : ""}</div>
     </SplitterLayout>
