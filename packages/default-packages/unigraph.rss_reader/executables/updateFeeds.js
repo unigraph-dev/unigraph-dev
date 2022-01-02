f53edba0e21902a37ddd790af9b744cdaff18b4a
Sophia Xu <sophia.xu@mail.mcgill.ca>
2022-01-02 12:26:49 -0500

    fix(rss): rss reader subscription and relative update time

--- packages/default-packages/unigraph.rss_reader/executables/updateFeeds.js
+++ packages/default-packages/unigraph.rss_reader/executables/updateFeeds.js
@@ -1,22 +1,23 @@
 const getQuery = (targetUrl, index) => `(func: uid(parType)) @cascade { 
-  uid
-  _value {
-    item_data {
-      _value {
+    uid
+    _value {
+      item_data {
         _value {
-          url {
-            _value @filter(eq(<_value.%>, "${targetUrl}"))
+          _value {
+            url {
+              _value @filter(eq(<_value.%>, "${targetUrl}"))
+            }
           }
         }
       }
     }
-  }
-}`;
+  }`;
 
 const queries = [];
 
 const feeds = (await unigraph.getType('$/schema/rss_feed')).map((el) => unpad(el));
 const urls = feeds.map((el) => el.feed_url);
+const latests = [];
 const Parser = require('rss-parser');
 
 const parser = new Parser({ timeout: 10000 });
@@ -29,16 +30,22 @@ const totalItems = await Promise.all(urls.map((el, index) => new Promise((resolv
         });
     });
 })));
-totalItems.forEach((items, i) => items.forEach((item, index) => {
-    queries.push({ query: getQuery(item.link, `${i}_${index}`), item, feedId: i });
-}));
+totalItems.forEach((items, i) => {
+    items.forEach((item, index) => {
+        if (!items[0].isoDate
+            || new Date(feeds[i].last_updated || 0).getTime() < new Date(items[0].isoDate).getTime()) {
+            queries.push({ query: getQuery(item.link, `${i}_${index}`), item, feedId: i });
+        }
+    });
+    latests[i] = new Date(Math.max(...items.map((el) => ((new Date(el.isoDate)).getTime() || 0)))).toISOString();
+});
 
 console.log('All items loaded!');
 const results = await unigraph.getQueries(queries.map((el) => el.query), undefined, 50, `var(func: eq(unigraph.id, "$/schema/rss_item")) {
-  <unigraph.origin> {
-    parType as uid
-  }
-}`);
+    <unigraph.origin> {
+      parType as uid
+    }
+  }`);
 console.log('Got current entities!');
 // console.log(results)
 const objects = results.map((els, index) => (els.length >= 1 ? undefined : {
@@ -62,6 +69,10 @@ const uids = await unigraph.addObject(objects, '$/schema/rss_item');
 const bookmarks = (await unigraph.getQueries(uids.map((el) => `(func: uid(${el})) {_value {item_data {_value {uid}}}}`)))
     .map((el) => el[0]?._value.item_data._value.uid)
     .filter((el) => el !== undefined);
+
+await Promise.all(latests.map((el, idx) => unigraph.updateObject(feeds[idx].uid, {
+    last_updated: el,
+})));
 
 if (uids.length) unigraph.runExecutable('$/package/unigraph.core/0.0.1/executable/add-item-to-list', { where: '$/entity/inbox', item: uids.reverse() });
 // TODO: fix this race condition by enforcing
