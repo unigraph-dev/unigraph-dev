f3e3128fe909a3c28aca2aed14c2982f9430cf4f
Sophia Xu <sophia.xu@mail.mcgill.ca>
2022-01-04 02:50:03 -0500

    fix(events): events should display everything in 24 hours

--- packages/default-packages/unigraph.calendar/executables/homeNextEvents.jsx
+++ packages/default-packages/unigraph.calendar/executables/homeNextEvents.jsx
@@ -1,48 +1,77 @@
 const [events, setEvents] = React.useState([]);
 
 React.useEffect(() => {
-    const fn = () => {
-        unigraph.runExecutable('$/executable/get-next-events', 
-            {
-                current: true, start: (new Date((new Date()).getTime() + 1000*60*60*24)).getTime(), 
-                greaterThanNow: true, allEnd: true
+    const fn = async () => {
+        const els = await unigraph.getQueries([`(func: uid(res)) @filter(type(Entity) AND (NOT type(Deleted)) AND (NOT eq(<_hide>, true))) @recurse {
+    uid
+    <unigraph.id>
+    expand(_userpredicate_)
+}
+frames as var(func: uid(partf)) @cascade {
+    _value {
+        end {
+            _value {
+                _value {
+                    datetime @filter(ge(<_value.%dt>, "${new Date().toJSON()}") AND le(<_value.%dt>, "${new Date(new Date().getTime() + 1000 * 60 * 60 * 24).toJSON()}")) {
+                        <_value.%dt>
+                    }
+                }
             }
-        ).then(async (query) => {
-            let els = await unigraph.getQueries([query]);
-            setEvents(els[0]);
-        })
+        }
+    }
+}
+var(func: eq(<unigraph.id>, "$/schema/time_frame")) {
+    <~type> {
+        partf as uid
+    }
+}
+var(func: uid(frames)) {
+    <unigraph.origin> {
+        res as uid
+    }
+}`]);
+        setEvents(els[0]);
     };
 
     const interval = setInterval(fn, 120000);
     fn();
 
-    return function cleanup () {
+    return function cleanup() {
         clearInterval(interval);
-    }
-
+    };
 }, []);
 
-return <React.Fragment>
-    <DynamicObjectListView items={events} groupBy={"time_frame"} groupers={{time_frame: (els) => {
+return (
+    <DynamicObjectListView
+        items={events}
+        groupBy="time_frame"
+        groupers={{
+ time_frame: (els) => {
         els = buildGraph(els);
-        let groups = {};
-        groups[Sugar.Date.medium(new Date())] = []
-        els.filter(el => el.type['unigraph.id'] === "$/schema/time_frame").forEach(el => {
+        const groups = {};
+        groups[Sugar.Date.medium(new Date())] = [];
+        els.filter((el) => el.type['unigraph.id'] === '$/schema/time_frame').forEach((el) => {
             const dd = Sugar.Date.medium(new Date((new UnigraphObject(el)).get('start/datetime').as('primitive')));
             if (groups[dd]) groups[dd].push(el);
             else groups[dd] = [el];
         });
         // 2. Go through groups and find all entities associated with these timeframes
-        let finalGroups = [];
+        const finalGroups = [];
         Object.entries(groups).sort((a, b) => Sugar.Date.create(a[0]).getTime() - Sugar.Date.create(b[0]).getTime()).map(([key, value]) => {
-            const insert = {name: key, items: []}
+            const insert = { name: key, items: [] };
             value.sort((a, b) => Sugar.Date.create(new Date((new UnigraphObject(a)).get('start/datetime').as('primitive'))).getTime() - Sugar.Date.create(new Date((new UnigraphObject(b)).get('start/datetime').as('primitive'))).getTime()).map((val) => {
-                els.filter(el => el.type['unigraph.id'] !== "$/schema/time_frame").forEach(el => {
+                els.filter((el) => el.type['unigraph.id'] !== '$/schema/time_frame').forEach((el) => {
                     if (JSON.stringify(unpad(el, false)).includes(val._value.uid)) insert.items.push(el);
-                })
-            })
-            finalGroups.push(insert)
+                });
+            });
+            finalGroups.push(insert);
         });
         return finalGroups;
-    }}} callbacks={{noDate: true}} context={null} noBar compact/>
-</React.Fragment>
+    },
+}}
+        callbacks={{ noDate: true }}
+        context={null}
+        noBar
+        compact
+    />
+);
