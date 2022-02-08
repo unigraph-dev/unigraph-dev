fc82338f7efbd0037d7f83cb7fa390530b1e7ba5
TheExGenesis <7385326+TheExGenesis@users.noreply.github.com>
2022-02-08 15:07:11 +0000

    refactor(calendar): remove comments

--- packages/unigraph-dev-explorer/src/examples/calendar/Calendar.tsx
+++ packages/unigraph-dev-explorer/src/examples/calendar/Calendar.tsx
@@ -52,7 +52,7 @@ export function CalendarEvent({ data, callbacks, inline }: any) {
                                   <AutoDynamicView
                                       object={new UnigraphObject(it._value)}
                                       callbacks={callbacks}
-                                      inline
+                                      options={{ inline: true }}
                                       style={{ verticalAlign: 'middle' }}
                                   />
                               ))
