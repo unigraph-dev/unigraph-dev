46803736ef37610924a5c41d6e4bc8f419380272
TheExGenesis <7385326+TheExGenesis@users.noreply.github.com>
2022-02-09 14:38:11 +0000

    feat(notes): paste url into selection notion-style

--- packages/unigraph-dev-explorer/src/examples/notes/NoteEditor.tsx
+++ packages/unigraph-dev-explorer/src/examples/notes/NoteEditor.tsx
@@ -261,12 +261,43 @@ export const useNoteEditor: (...args: any) => [any, (text: string) => void, () =
         [checkReferences, data],
     );
 
+    const pasteLinkIntoSelection = React.useCallback((url: string) => {
+        // console.log(document.getSelection())
+        const caret = textInputRef.current.selectionStart;
+        let middle = document.getSelection()?.toString() || '';
+        let end = '';
+        if (middle.endsWith(' ')) {
+            middle = middle.slice(0, middle.length - 1);
+            end = ' ';
+        }
+        setCurrentText(
+            `${getCurrentText().slice(0, caret)}[${middle}](${url})${end}${getCurrentText().slice(
+                caret + (middle + end).length,
+            )}`,
+        );
+        setCaret(document, textInputRef.current, caret + middle.length + url.length + 4);
+
+        textInputRef.current.dispatchEvent(
+            new Event('change', {
+                bubbles: true,
+                cancelable: true,
+            }),
+        );
+    }, []);
+
     const onPasteHandler = React.useCallback(
         (event) => {
             const paste = (event.clipboardData || (window as any).clipboardData).getData('text/html');
 
             const img = event.clipboardData.items[0];
-            if (paste.length > 0) {
+            const text = event.clipboardData.getData('text');
+            if (isUrl(text)) {
+                // Notion-style paste url into selection
+                const selection = window.getSelection();
+                if (!selection?.rangeCount) return false;
+                pasteLinkIntoSelection(text);
+                event.preventDefault();
+            } else if (paste.length > 0) {
                 const selection = window.getSelection();
                 if (!selection?.rangeCount) return false;
                 selection?.deleteFromDocument();
