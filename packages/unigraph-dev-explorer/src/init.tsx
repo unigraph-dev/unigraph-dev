4abf609d1b95f73de358bd97bfadd08a5b79e15e
Sophia Xu <sophia.xu@mail.mcgill.ca>
2022-03-31 15:15:23 -0400

    chore(reddit): move reddit package to userspace

--- packages/unigraph-dev-explorer/src/init.tsx
+++ packages/unigraph-dev-explorer/src/init.tsx
@@ -20,7 +20,6 @@ import { init as bmInit } from './examples/bookmarks/Bookmarks';
 import { init as clInit } from './examples/calendar/init';
 import { init as emInit } from './examples/email/Email';
 import { init as nbInit } from './examples/notes/init';
-import { init as reInit } from './examples/reddit/RedditPost';
 import { init as rssInit } from './examples/rss_reader/RSSFeeds';
 import { init as smInit } from './examples/semantic/init';
 import { init as tdInit } from './examples/todo/TodoList';
@@ -340,7 +339,6 @@ function initPackages() {
     nbInit();
     smInit();
     twInit();
-    reInit();
     clInit();
     pbInit();
 }
