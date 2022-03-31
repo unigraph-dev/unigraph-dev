728f918be96e81fa7a7225a13f30b91a50014ac9
Alexander Wang <zxcvb22217@gmail.com>
2022-03-31 18:30:26 +0800

    feat(outliner): make DragHandle easier to click

--- packages/unigraph-dev-explorer/src/examples/notes/DragHandle.tsx
+++ packages/unigraph-dev-explorer/src/examples/notes/DragHandle.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const DragHandleContainer = styled('div')({
     flex: '0 0 1rem',
     width: '1rem',
-    height: '1rem',
+    height: '1.2rem',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -23,13 +23,13 @@ const DragHandleContainer = styled('div')({
 export const DragHandle = React.forwardRef<HTMLDivElement, React.ComponentPropsWithoutRef<'div'>>((props, ref) => {
     return (
         <DragHandleContainer ref={ref} {...props}>
-            <svg width="100%" height="100%" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="9" cy="7" r="1.5" />
-                <circle cx="9" cy="12" r="1.5" />
-                <circle cx="9" cy="17" r="1.5" />
-                <circle cx="15" cy="7" r="1.5" />
-                <circle cx="15" cy="12" r="1.5" />
-                <circle cx="15" cy="17" r="1.5" />
+            <svg width="100%" height="100%" viewBox="0 0 20 24" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="7" cy="7" r="1.5" />
+                <circle cx="7" cy="12" r="1.5" />
+                <circle cx="7" cy="17" r="1.5" />
+                <circle cx="13" cy="7" r="1.5" />
+                <circle cx="13" cy="12" r="1.5" />
+                <circle cx="13" cy="17" r="1.5" />
             </svg>
         </DragHandleContainer>
     );
