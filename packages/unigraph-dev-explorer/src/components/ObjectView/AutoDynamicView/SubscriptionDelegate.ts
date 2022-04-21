b486c16f32eef0c931b4c0dfbf931dd7179abf0c
Sophia Xu <sophia.xu@mail.mcgill.ca>
2022-04-21 11:22:09 -0400

    feat(frontend): add object subscription cache to speedup

--- packages/unigraph-dev-explorer/src/components/ObjectView/AutoDynamicView/SubscriptionDelegate.ts
+++ packages/unigraph-dev-explorer/src/components/ObjectView/AutoDynamicView/SubscriptionDelegate.ts
@@ -3,6 +3,22 @@ import { getRandomInt, UnigraphObject } from 'unigraph-dev-common/lib/utils/util
 import { TabContext } from '../../../utils';
 import { isStub } from '../utils';
 
+const getQueryFromObjectView = (objectView: any) => {
+    let query = objectView?.query?.('QUERYFN_TEMPLATE');
+    if (!query) {
+        query = `(func: uid(QUERYFN_TEMPLATE)) @recurse {
+        uid
+        unigraph.id
+        expand(_userpredicate_)
+        }`;
+    }
+    return query;
+};
+
+const getSubsCache = () => window.unigraph.getState('global/subscriptionCache');
+const getKey = (uid: string, objectView: any) => `${uid}_${getQueryFromObjectView(objectView)}`;
+const getObjectCache = (uid: string, objectView: any) => getSubsCache().value[getKey(uid, objectView)];
+
 export const useSubscriptionDelegate: (...args: any) => [() => any, number] = (
     uid: string,
     objectView: any,
@@ -15,7 +31,16 @@ export const useSubscriptionDelegate: (...args: any) => [() => any, number] = (
     const [subsId, setSubsId] = React.useState(0);
 
     const getObject = React.useCallback(() => {
-        let obj = isObjectStub ? loadedObj : object;
+        let obj;
+        if (isObjectStub) {
+            if (subsId === 0 && loadedObj === false && getObjectCache(uid, objectView)) {
+                obj = getObjectCache(uid, objectView);
+            } else {
+                obj = loadedObj;
+            }
+        } else {
+            obj = object;
+        }
         if (obj?.constructor.name !== 'UnigraphObject' && obj?.uid) obj = new UnigraphObject(obj);
         return obj;
     }, [loadedObj, object, isObjectStub]);
@@ -29,14 +54,7 @@ export const useSubscriptionDelegate: (...args: any) => [() => any, number] = (
         if (isObjectStub && object?.uid !== uidRef.current) {
             // console.log(tabContext);
             // if (subsId) tabContext.unsubscribe(subsId);
-            let query = objectView?.query?.('QUERYFN_TEMPLATE');
-            if (!query) {
-                query = `(func: uid(QUERYFN_TEMPLATE)) @recurse {
-                uid
-                unigraph.id
-                expand(_userpredicate_)
-              }`;
-            }
+            const query = getQueryFromObjectView(objectView);
             tabContext.subscribe(
                 {
                     type: 'object',
@@ -47,6 +65,7 @@ export const useSubscriptionDelegate: (...args: any) => [() => any, number] = (
                 },
                 (newObjects: any[]) => {
                     uidRef.current = object?.uid;
+                    getSubsCache().setValue({ ...getSubsCache().value, [getKey(uid, objectView)]: newObjects[0] });
                     setLoadedObj(newObjects[0]);
                 },
                 newSubs,
